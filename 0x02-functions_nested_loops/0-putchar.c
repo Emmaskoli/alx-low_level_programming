30ab3338064230222a8de8096f2e01ecb546b9f1
Emmaskoli <emmaskoli4life@yahoo.com>
2022-12-13 14:00:28 -0800

    Print putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,16 +7,18 @@
  */
 
 int main(void)
+
 {
-	_putchat("_");
-	_putchat("p");
-	_putchat("u");
-	_putchat("t");
-	_putchat("c");
-	_putchat("h");
-	_putchat("a");
-	_putchat("r");
-	_putchat("\n");
+
+	_putchat('_');
+	_putchat('p');
+	_putchat('u');
+	_putchat('t');
+	_putchat('c');
+	_putchat('h');
+	_putchat('a');
+	_putchat('r');
+	_putchat('\n');
 
 	return (0);
 }
