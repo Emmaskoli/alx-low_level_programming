bfe25e47e88ccd94162197efcea8d422760cfde5
Emmaskoli <emmaskoli4life@yahoo.com>
2022-12-13 12:28:50 -0800

    A code that prints _putchar and then create a new line

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	_putchat("_");
+	_putchat("p");
+	_putchat("u");
+	_putchat("t");
+	_putchat("c");
+	_putchat("h");
+	_putchat("a");
+	_putchat("r");
+	_putchat("\n");
+
+	return (0); 
+}
